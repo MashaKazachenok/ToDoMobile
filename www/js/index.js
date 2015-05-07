c09a9de97f613c63f05a49343b3dafc55399ce7e
MashaKazachenok <kazachenokm@mail.ru>
2015-05-07 10:00:20 +0300

    add angular

--- www/js/index.js
+++ www/js/index.js
@@ -1,215 +1,42 @@
+function TodoCtrl($scope){
 
-var tasks = [];
+    $scope.todoList =
+        [
+            { text:'go', done:false }
 
-function vvod() {
-    if(event.keyCode == 13){
-        addClickHandler();
-        displayPodval();
-        getActiveTaskCount();
-        getCompletedTaskCount();
+        ];
+
+    $scope.addTask = function()
+    {
+        $scope.todoList.push({text:$scope.newTask, done:false});
+        $scope.newTask= "";
     };
-}
 
-function addClickHandler() {
-    var description = getDescription();
-    tasks.push(description);
-    displayTasks(tasks);
-}
+    $scope.clearCompleted = function()
+    {
+        $scope.todoList = _.filter($scope.todoList, function(todo){
+            return !todo.done;
+        });
+    };
 
-function getDescription() {
-    return getInput().value;
-}
+    $scope.getActive = function(){
 
 
-function getInput() {
-    return document.getElementById('task');
-}
+    };
 
-function getContainer() {
-    return document.getElementById('list');
-}
+    $scope.getCompleted = function()
+    {
 
 
-function clearClickHandler(argument) {
-    var container = getContainer();
-    container.innerHTML = null;
-}
+    };
 
-function showAll(event) {
-    setInactiveStyleAllButton();
-    var but = event.target;
-    but.className = "act";
-    var activeTasks = arrGetActive();
-    setDisplayForTasks(activeTasks, "");
-    var completedTasks = arrGetCompleted();
-    setDisplayForTasks(completedTasks, "");
-}
+    $scope.getAllTask = function()
+    {
+        return $scope.todoList.length;
+    };
 
-function arrGetActive (event) {
-
-
-    var container = getContainer();
-    var tasks = container.children;
-    var activeTasks = [];
-
-    for (var i = 0; i < tasks.length; i++) {
-        var task = tasks[i];
-        var completed = task.children[0].checked;
-
-        if (!completed)
-        {
-            activeTasks.push(task);
-        }
-    }
-
-    return activeTasks;
-}
-
-function arrGetCompleted (event) {
-
-    var container = getContainer();
-    var tasks = container.children;
-    var completedTasks = [];
-
-    for (var i = 0; i < tasks.length; i++) {
-        var task = tasks[i];
-        var completed = task.children[0].checked;
-
-        if (completed)
-        {
-            completedTasks.push(task);
-        }
-    }
-
-    return completedTasks;
-}
-
-function showActive(event) {
-    setInactiveStyleAllButton();
-    but = event.target;
-    but.className = "act";
-    var activeTasks = arrGetActive();
-    //activeTasks.className = "act";
-    //console.log(activeTasks);
-    setDisplayForTasks(activeTasks, "");
-
-    var completedTasks = arrGetCompleted();
-    setDisplayForTasks(completedTasks, "none");
 
 }
-
-function setDisplayForTasks(tasks, display) {
-    for (var i = 0; i < tasks.length; i++) {
-        tasks[i].style.display = display;
-    }
-}
-
-function setInactiveStyleAllButton () {
-    var activeButton =  document.querySelector('input.act');
-
-    if (activeButton) {
-        activeButton.className = "Btn";
-    }
-}
-
-function showCompleted() {
-    setInactiveStyleAllButton();
-    var but = event.target;
-    but.className = "act";
-    var activeTasks = arrGetActive();
-    setDisplayForTasks(activeTasks, "none");
-
-    var completedTasks = arrGetCompleted();
-    setDisplayForTasks(completedTasks, "");
-}
-
-function clearCompleted() {
-    var container = getContainer();
-    var completedTasks = arrGetCompleted();
-
-    // удаляем завершенные задачи
-    for (var i = 0; i < completedTasks.length; i++) {
-        container.removeChild(completedTasks[i])
-    }
-    displayPodval();
-}
-
-
-function del(event) {
-    var task = event.target.parentNode;
-    var container = task.parentNode;
-    container.removeChild(task);
-    getActiveTaskCount()
-    displayPodval();
-}
-
-function changeStatus(event) {
-    var task = event.target.parentNode;
-    var desription = task.children[1];
-
-    var status = event.target;
-
-    task.className = (status.checked) ? "completed" : "active";
-
-    getActiveTaskCount();
-    getCompletedTaskCount();
-}
-
-function displayTasks(tasks) {
-
-    var taskDescription = tasks[tasks.length-1];
-
-    var changeStatusAction = document.createElement('input');
-    changeStatusAction.type = "checkbox";
-    changeStatusAction.addEventListener("click", changeStatus);
-
-    var description = document.createElement('span');
-    description.innerHTML = taskDescription;
-
-    var deleteAction = document.createElement('input');
-    deleteAction.type = "button";
-    deleteAction.value = "x";
-    //delete
-    deleteAction.addEventListener("click", del);
-
-    var task = document.createElement('p');
-    task.className = "active";
-    task.appendChild(changeStatusAction);
-    task.appendChild(description);
-    task.appendChild(deleteAction);
-
-    var container = getContainer();
-    container.appendChild(task);
-
-}
-
-function  getActiveTaskCount() {
-
-    var activeTasks = arrGetActive();
-
-    document.getElementById('count').innerHTML = activeTasks.length;
-}
-
-function  getCompletedTaskCount() {
-
-    var completedTasks = arrGetCompleted();
-
-    var description = document.getElementById('clearCompleted').attributes['data-description'].value;
-
-    document.getElementById('clearCompleted').value = description + completedTasks.length;
-}
-
-function displayPodval () {
-
-    var container = getContainer();
-    var tasks = container.children;
-
-    var display = (tasks.length > 0) ? '' : 'none';
-
-    document.getElementById('podval').style.display = display;
-}
-
-
 
 window.onload = function(){
     getInput().addEventListener("keydown", vvod);
@@ -234,7 +61,8 @@ window.onload = function(){
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         //document.addEventListener('DOMContentLoaded', this.onDeviceReady, false);
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+      //  document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('DOMContendLoaded', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
